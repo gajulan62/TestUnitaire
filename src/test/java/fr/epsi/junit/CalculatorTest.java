3afaaf8c60a9d2db90a6945e125fc35e282d7de7
gajulan62 <83352049+gajulan62@users.noreply.github.com>
2021-06-21 12:28:29 +0200

    push2

--- src/test/java/fr/epsi/junit/CalculatorTest.java
+++ src/test/java/fr/epsi/junit/CalculatorTest.java
@@ -9,11 +9,17 @@ public class CalculatorTest {
     private Calculator calculator;
     @BeforeEach
     public void init(){
+        system.out.printl("----------------");
+        system.out.printl("Iitialisation apres chaque test");
         this.calculator = new Calculator ();
 
     }
+    @Test
     public void should_add_two_numbers(){
-       // TODO
+       int A=2;
+       int B=3;
+
+        Assertions.assertEquals( Calculator.add(A,B));
     }
 
     public void should_substract_two_numbers(){
