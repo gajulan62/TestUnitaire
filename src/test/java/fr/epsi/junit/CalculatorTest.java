b7d86ab38768f188ef44eba4f8f2c2ec526f8dbb
gajulan62 <83352049+gajulan62@users.noreply.github.com>
2021-06-23 10:54:48 +0200

    thanck

--- src/test/java/fr/epsi/junit/CalculatorTest.java
+++ src/test/java/fr/epsi/junit/CalculatorTest.java
@@ -7,45 +7,52 @@ import org.junit.jupiter.api.Test;
 public class CalculatorTest {
 
     private Calculator calculator;
+    private Assertions Assert;
+
     @BeforeEach
-    public void init(){
+    public void init() {
         System.out.println("----------------");
         System.out.println("Initialisation après chaque tests");
-        this.calculator = new Calculator ();
-            }
-            @Test
-            public void should_add_two_numbers(){
-               int a=2;
-               int b=3;
+        this.calculator = new Calculator();
+    }
 
-                short result = 0;
-                Assertions.assertEquals(result, Calculator.add(a,b));
-            }
-            @Test
-            public void should_multi_two_numbers(){
-                int a=3;
-                int b=2;
+    @Test
+    public void should_add_two_numbers() {
+        int a = 2;
+        int b = 3;
 
-                short result = 0;
-                Assertions.assertEquals(result, Calculator.multi(a*b));
-            }
-            @Test
-            public void should_substract_two_numbers(){
-                    int a=3;
-                    int b=2;
+        int result = a+b;
+        Assertions.assertEquals(result, Calculator.add(a, b));
 
-                    short result = 0;
-                    Assertions.assertEquals(result, Calculator.substract(a-b));
-             }
-            @Test
-            public void should_div_two_numbers(){
-                    int A=3;
-                    int B=2;
+    }
 
-                    short result = 0;
-                    Assertions.assertEquals(result, Calculator.div(A/B));
-            }
+    @Test
+    public void should_substract_two_numbers() {
+        int a = 3;
+        int b = 2;
 
+        int result = a-b;
+        Assertions.assertEquals(result, Calculator.substract(a, b));
+    }
+
+    @Test
+    public void should_multi_two_numbers() {
+        int a = 3;
+        int b = 2;
+
+        int result = a*b;
+        Assertions.assertEquals(result, Calculator.multi(a, b));
+    }
+
+    @Test
+    public void should_div_two_numbers() {
+        int a = 3;
+        int b = 2;
+
+        int result = a/b;
+        Assertions.assertEquals(result, Calculator.div(a, b));
+
+    }
 }
 
 
